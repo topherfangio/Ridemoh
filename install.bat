c150b98447bec900cac818e8c8b0088d766674d4
Topher Fangio <fangiotophia@gmail.com>
2015-01-19 09:55:36 +0100

    Create install.bat
    Add an easy installation method for Windows.

---
+++ install.bat
@@ -0,0 +1,21 @@
+@echo off
+rem
+rem Quickly install to the requested directory
+rem
+
+rem If the first argument is NOT blank (i.e. they passed an argument)
+IF NOT "%1"=="" (
+  echo "Installing files to %1..."
+
+  cp -r .vim %1
+  cp .bashrc %1
+  cp .profile %1
+  cp .screenrc %1
+  cp .screenrc.programming %1
+  cp .screenrc.server %1
+  cp .vimrc %1
+
+  echo "Done!"
+) ELSE (
+  echo "Usage: install.bat /path/to/directory"
+)
